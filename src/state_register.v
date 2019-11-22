4658b68b68cb470b7c9c89b9f985a42570d6f552
Eric Yeats <eric.c.yeats@vanderbilt.edu>
2019-11-22 10:50:35 -0500

    added v, u state register

---
+++ src/state_register.v
@@ -0,0 +1,9 @@
+// Eric Yeats / Brady Taylor
+// ECE 552 Advanced Computer Architecture I
+// Final Project
+// State Register
+
+
+// This module will store the v, u state for each
+// neuron in the simulation. The state information
+// will be addressable by neuron tag.
