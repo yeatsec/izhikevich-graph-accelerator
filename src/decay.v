309f91dfc56a024362551842660eca702d211074
Brady <bgt8@duke.edu>
2019-11-23 12:43:09 -0500

    Pushing so we can synch up

--- src/decay.v
+++ src/decay.v
@@ -2,37 +2,39 @@
 module decay (clk, write, decay, curr_in1, curr_in2, i_prime1, i_prime2, i1, i2);
 
 // Outputs
-output [15:0] i1, i2;
+output [16:0] i1, i2, i_prime1, i_prime2;
 
 // Inputs
 input clk, write;
-input [15:0] decay, curr_in1, curr_in2, i_prime1, i_prime2;
+input [16:0] decay, curr_in1, curr_in2;
 
 // Wire declarations
 wire clk, write;
-wire [15:0] decay, curr_in1, curr_in2;
+wire [16:0] decay, curr_in1, curr_in2;
 
 // Register declarations
-reg [15:0] i1, i2, i_prime1, i_prime2;
+reg [16:0] i1, i2, i_prime1, i_prime2;
 
 // Internel signals
-reg [15:0] buff1, buff2;
+wire [16:0] buff1, buff2;
+
+// Keep multiplication of I on hand to eventually assign to I'
+fixed_mult U1(i1, decay, buff1);
+fixed_mult U2(i2, decay, buff2);
 
 // Sequential logic
 always @ (posedge clk) begin
 	if (write == 1'b1) begin
-		i_prime1 <= curr_in1;
-		i_prime2 <= curr_in2;
-		i1 <= 1'b0;
-		i2 <= 1'b0;
+		i_prime1 <= buff1;
+		i_prime2 <= buff2;
+		i1 <= curr_in1;
+		i2 <= curr_in2;
 	end
 	else begin
-		buff1 = i1*decay;
-		buff2 = i2*decay;
-		i1 = i_prime1;
-		i2 = i_prime2;
-		i_prime1 = buff1;
-		i_prime2 = buff2;
+		i1 <= i_prime1;
+		i2 <= i_prime2;
+		i_prime1 <= buff1;
+		i_prime2 <= buff2;
 	end
 end
 
