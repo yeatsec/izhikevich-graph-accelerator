675d8ca74ec1a2c1c25920714f1f0b2ca1c47e88
Eric Yeats <eric.c.yeats@vanderbilt.edu>
2019-11-25 13:21:38 -0500

    updated SPU, rewrote fifo

---
+++ src/integration/fifo.v
@@ -0,0 +1,82 @@
+// Eric Yeats / Brady Taylor
+// ECE 552 Advanced Computer Architecture I
+// Final Project
+// FIFO
+
+// Redoing the Fire Fifo
+
+module fifo(clk, enq, deq, asyn_reset, in_tag, out_tag, full, empty);
+
+    parameter numneurons = 2;
+    parameter tagbits = 1;
+
+    input clk, enq, deq, asyn_reset;
+    input [tagbits-1:0] in_tag;
+    output [tagbits-1:0] out_tag;
+    output full, empty;
+
+    wire clk, enq, deq, asyn_reset, full, empty;
+
+    reg [tagbits-1:0] mem [0:numneurons-1];
+    reg [tagbits-1:0] ptr;
+
+    wire [tagbits-1:0] ptr_next;
+    reg isfull, isempty;
+    wire isfull_next, isempty_next;
+    reg [numneurons:0] j;
+
+    // state update
+    always @(posedge clk, posedge asyn_reset) begin
+
+        if (asyn_reset) begin 
+            for (j=0; j<numneurons; j=j+1)
+                mem[j] <= 0;
+            ptr <= 0;
+            isfull <= 1'b0;
+            isempty <= 1'b1;
+        end else begin
+            if (!isfull)
+                mem[ptr] = in_tag;
+            else
+                mem[ptr] = mem[ptr];
+            ptr = ptr_next;
+            isfull <= isfull_next;
+            isempty <= isempty_next;
+        end
+    end
+
+    // state change logic
+    always @(*) begin
+        if (enq && deq && !isempty && !isfull) begin
+            ptr_next <= ptr;
+            isfull_next <= isfull;
+            isempty_next <= isempty;
+        end else if (enq && !deq && !isfull) begin
+            ptr_next <= ptr + 1;
+            if (ptr_next == 0) // will evaluate correctly once ptr_next updates
+                isfull_next <= 1'b1;
+            else
+                isfull_next <= 1'b0;
+            isempty_next <= 1'b0; // cannot be empty
+        end else if (!enq && deq && !isempty) begin
+            ptr_next <= ptr - 1;
+            if (ptr_next == 0) // will evaluate correctly once ptr_next updates
+                isempty_next <= 1'b1;
+            else
+                isempty_next <= 1'b0;
+            isfull_next <= 1'b0; // cannot be full
+        end else begin // default
+            ptr_next <= ptr;
+            isfull_next <= isfull;
+            isempty_next <= isempty;
+        end
+    end
+
+    // outputs
+    always @(*) begin
+        full <= isfull;
+        empty <= isempty;
+        out_tag <= mem[0];
+    end
+
+endmodule
--- src/synaptic_processing_unit.v
+++ src/synaptic_processing_unit.v
@@ -14,7 +14,7 @@
 // STATE fetch_src OUT busy, req_deq EDGE (fetch_dst_wgt_inext)
 // STATE fetch_dst_wgt_inext OUT busy
 
-module synaptic_processing_unit(clk, asyn_reset, fifo_empty, req_deq, req_write_i_next, weight_in, i_next_in, i_next_out, src_tag_in, src_tag_out, dst_tag_out);
+module synaptic_processing_unit(clk, asyn_reset, fifo_empty, req_deq, req_write_i_next, busy, weight_in, i_next_in, i_next_out, src_tag_in, src_tag_out, dst_tag_out);
 
     parameter numneurons = 2;
     parameter numwidth = 16;
@@ -32,80 +32,119 @@ module synaptic_processing_unit(clk, asyn_reset, fifo_empty, req_deq, req_write_
     input [numwidth:0] weight_in, i_next_in;
     input [tagbits-1:0] src_tag_in;
 
-    output req_deq, req_write_i_next;
+    output req_deq, req_write_i_next, busy;
     output [numwidth:0] i_next_out;
     output [tagbits-1:0] src_tag_out, dst_tag_out;
 
-    wire clk, asyn_reset, fifo_empty, req_deq;
+    wire clk, asyn_reset, fifo_empty, req_deq, busy;
     wire [numwidth:0] weight_in, i_next_in, i_next_out;
-    wire [tagbits-1:0] src_tag_in, src_tag_out, dst_tag_out;
+    wire [tagbits-1:0] src_tag_in, src_tag_out, dst_tag_out, dst_tag_next;
 
-    reg [tagbits-1:0] src_tag_reg, dst_tag_reg, src_done;
+    reg [tagbits-1:0] src_tag_reg, dst_tag_reg;
     reg [numwidth:0] cur_i_next, cur_weight, result;
-    reg [numstates-1:0] state;
+    reg [numstates-1:0] state, state_next;
 
+    // State Update
     always @(posedge clk, posedge asyn_reset) begin
         if (asyn_reset) begin
-            src_tag_reg[tagbits-1:0] <= 1'b0;
-            dst_tag_reg[tagbits-1:0] <= 1'b0;
+            src_tag_reg <= 0;
+            dst_tag_reg <= 0;
             src_done <= 1'b0;
             state <= wait_src;
         end
         else begin
-            // fsm next state logic
-            case(state)
-                wait_src: begin 
-                    // outputs
-                    req_write_i_next <= 1'b0;
-                    req_deq <= 1'b0;
-                    if (!fifo_empty) begin
-                        state <= #1 fetch_src;
-                    end else begin
-                        state <= #1 wait_src;
-                    end
-                end
-                fetch_src: begin
-                    // state deterministic
-                    state <= #1 fetch_dst_wgt_inext;
-                    src_tag_reg <= #1 src_tag_in; // add delay cuz don't need yet
-                    // initialize dst_tag_reg to zero
-                    dst_tag_reg <= 0;
-                    // outputs
-                    req_deq <= 1'b1; // using the value of tag next cycle
-                    req_write_i_next <= 1'b0;
-                end
-                fetch_dst_wgt_inext: begin
-                    state <= #1 add_wgt_inext;
-                    // grab the values for addition
-                    cur_i_next <= i_next_in;
-                    cur_weight <= weight_in;
-                    // outputs
-                    req_deq <= 1'b0;
-                    req_write_i_next <= 1'b0;
-                end
-                add_wgt_inext: begin
-                    state <= #1 write_inext; // perform signed addition
-                    result <= // need a fixed signed add module to do this work
-                    req_deq <= 1'b0;
-                    req_write_i_next <= 1'b0;
-                end
-                write_inext: begin
-                    // broadcast addition result
-                    req_write_i_next <= 1'b1;
-                    // other outputs
-                    req_deq <= 1'b0;
-                    // calculate next dst_tag
-                    dst_tag_reg <= dst_tag_reg + 1; // does this overflow scheme work?
-                    if (dst_tag_reg == 0) begin // go to wait, no more dst tags
-                        state <= #1 wait_src;
-                    end else begin
-                        state <= #1 fetch_dst_wgt_inext;
-                    end
-                end
-            endcase          
+            dst_tag_reg <= dst_tag_next;
+            state <= state_next;
         end
     end
-    assign src_tag_out = src_tag_reg;
-    assign dst_tag_out = dst_tag_reg;
-    // TODO - assign i_next_out to be value of adder
+
+    // FSM next state logic
+    always @(*) begin
+        case(state)
+            wait_src: begin 
+                dst_tag_next <= dst_tag_reg;
+                src_tag_reg <= src_tag_reg;
+                cur_i_next <= cur_i_next;
+                cur_weight <= cur_weight;
+                result <= result;
+                if (!fifo_empty) begin 
+                    state_next <= fetch_src;
+                end else begin
+                    state_next <= wait_src;
+                end
+            end
+            fetch_src: begin
+                state_next <= fetch_dst_wgt_inext;
+                dst_tag_next <= dst_tag_reg;
+                src_tag_reg <= src_tag_in; // latch
+                cur_i_next <= cur_i_next;
+                cur_weight <= cur_weight;
+                result <= result;
+            end
+            fetch_dst_wgt_inext: begin
+                state_next <= add_wgt_inext;
+                dst_tag_next <= dst_tag_reg;
+                src_tag_reg <= src_tag_reg;
+                cur_i_next <= i_next_in; // latch
+                cur_weight <= weight_in; // latch
+                result <= result;
+            end
+            add_wgt_inext: begin
+                state_next <= write_inext; // perform signed addition
+                dst_tag_next <= dst_tag_reg;
+                src_tag_reg <= src_tag_reg;
+                cur_i_next <= cur_i_next;
+                cur_weight <= cur_weight;
+                result <= // signed addition here
+            end
+            write_inext: begin
+                dst_tag_next <= dst_tag_reg + 1;
+                src_tag_reg <= src_tag_reg;
+                cur_i_next <= cur_i_next;
+                cur_weight <= cur_weight;
+                result <= result;
+                if (dst_tag_next == 0) begin // go to wait, no more dst tags
+                    state_next <= wait_src;
+                end else begin
+                    state_next <= fetch_dst_wgt_inext;
+                end
+            end
+        endcase
+    end
+
+    // FSM output
+    always @(*) begin
+        i_next_out <= result; // result always up
+        src_tag_out <= src_tag_reg;
+        dst_tag_out <= dst_tag_reg;
+        case (state)
+            wait_src: begin
+                busy <= 1'b0;
+                req_deq <= 1'b0;
+                req_write_i_next <= 1'b0;
+            end
+            fetch_src: begin
+                busy <= 1'b1;
+                req_deq <= 1'b1; // FIFO state change
+                req_write_i_next <= 1'b0;
+            end
+            fetch_dst_wgt_index: begin
+                busy <= 1'b1;
+                req_deq <= 1'b0;
+                req_write_i_next <= 1'b0;
+            end
+            add_wgt_inext: begin
+                busy <= 1'b1;
+                req_deq <= 1'b0;
+                req_write_i_next <= 1'b0;
+            end
+            write_inext: begin
+                busy <= 1'b1;
+                req_deq <= 1'b0;
+                req_write_i_next <= 1'b1; // current_update state
+            end
+        endcase
+    end
+        
+
 endmodule
